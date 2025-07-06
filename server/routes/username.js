012adb40eb670833a453dd9f55244ae2d10b387d
SACHIN BURNWAL <sachinburnwal22@gmail.com>
2025-07-06 22:48:26 +0530

    Updated homePage and finalized Username feature

--- server/routes/username.js
+++ server/routes/username.js
@@ -9,6 +9,24 @@ const platforms = {
   twitter: (username) => `https://twitter.com/${username}`,
   instagram: (username) => `https://instagram.com/${username}`,
   reddit: (username) => `https://www.reddit.com/user/${username}`,
+  pinterest: (username) => `https://www.pinterest.com/${username}`,
+  devto: (username) => `https://dev.to/${username}`,
+  medium: (username) => `https://medium.com/@${username}`,
+  tumblr: (username) => `https://${username}.tumblr.com`,
+  soundcloud: (username) => `https://soundcloud.com/${username}`,
+  vimeo: (username) => `https://vimeo.com/${username}`,
+  producthunt: (username) => `https://www.producthunt.com/@${username}`,
+  aboutme: (username) => `https://about.me/${username}`,
+  githubgist: (username) => `https://gist.github.com/${username}`,
+  codepen: (username) => `https://codepen.io/${username}`,
+  replit: (username) => `https://replit.com/@${username}`,
+  flipboard: (username) => `https://flipboard.com/@${username}`,
+  slideshare: (username) => `https://slideshare.net/${username}`,
+  wattpad: (username) => `https://www.wattpad.com/user/${username}`,
+  goodreads: (username) => `https://www.goodreads.com/user/show/${username}`,
+  behance: (username) => `https://www.behance.net/${username}`,
+  dribbble: (username) => `https://dribbble.com/${username}`,
+  stackoverflow: (username) => `https://stackoverflow.com/users/${username}`,
 };
 
 router.post("/username-scan", async (req, res) => {
@@ -21,8 +39,8 @@ router.post("/username-scan", async (req, res) => {
       const url = urlFn(username);
       try {
         const response = await axios.get(url);
-        result[platform] = { found: true, url };
-      } catch (err) {
+        result[platform] = { found: response.status === 200, url };
+      } catch {
         result[platform] = { found: false };
       }
     })
